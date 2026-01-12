b987540aca466cc181a1cf876dc1aa8328c197c1
mdaasim77 <khanaasim9910@gmail.com>
2026-01-12 08:05:50 +0530

    REvising JS

---
+++ Rough.js
@@ -0,0 +1,24 @@
+// console.log(0.1 + 0.2 === 0.3); // false floating precision 0.30000000000004
+// console.log(0.1 + 0.2 >= 0.3); // true
+
+// const obj = {
+//   a: 1,
+//   b: 2,
+// };
+
+// console.log(obj);
+
+// let str = JSON.stringify(obj);
+
+// console.log(str);
+
+// let num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+// let dbl = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+// let newe = num.reduce((acc, curr) => acc + curr, 0);
+
+// let dob = dbl.map((e) => e * 2).reduce((acc, curr) => acc + curr, 0);
+
+// console.log(newe - 12);
+// console.log(dob - 12);
+// console.log(dob + newe - 25);
